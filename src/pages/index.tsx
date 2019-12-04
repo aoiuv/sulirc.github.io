e1355730892d4952226601c0e160216fd048e8a0
严广杰 <yanguangjie@cvte.com>
2019-12-04 14:18:41 +0800

    modify: update config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './index.css';
 import { formatMessage } from 'umi-plugin-locale';
+
 export default function() {
   return (
     <div className={styles.normal}>
